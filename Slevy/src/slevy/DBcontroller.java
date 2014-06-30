2bae4ac68bd9f146493d067e62f5912b3fbfa3b1
martinhudec.mh <martinhudec.mh@8fa76b3b-60bf-4dd9-71eb-f231235fabac>
2014-06-30 16:04:22 +0000

    added new benefits (Sphere), code refactor

--- Slevy/src/slevy/DBcontroller.java
+++ Slevy/src/slevy/DBcontroller.java
@@ -1,9 +1,4 @@
 package slevy;
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -11,7 +6,6 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Properties;
@@ -19,24 +13,22 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- *
+ * Trieda na prácu s databázou, pripojenie k nej, vyplnenie,..
  * @author martin
  */
 public class DBcontroller {
-
+    
     private final String URL = "jdbc:postgresql://ec2-54-197-251-18.compute-1.amazonaws.com:5432/d1gcms75s3ef0g?user=nxxclbdnrjiwcx&password=pz1YC2j_lfM4YBuV3iLFyV7c_p&ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory";
     private final Properties PROPS = new Properties();
 
-    //Settings for connection to the DB 
-    public boolean fillDB(List<GPSrecord> list) throws ClassNotFoundException {
+    // Nastavenia pre pripojenie k databáze
+    public boolean fillDB(List<Benefit> list) throws ClassNotFoundException {
         Class.forName("org.postgresql.Driver");
         Connection connection = null;
         PROPS.setProperty("user", "nxxclbdnrjiwcx");
         PROPS.setProperty("password", "pz1YC2j_lfM4YBuV3iLFyV7c_p");
         PROPS.setProperty("ssl", "true");
         try {
-            // ========>     from heroku website
-            //String url = "jdbc:postgresql://ec2-107-20-214-225.compute-1.amazonaws.com:5432/d6iv2kl28367ds?user=wkpftjetpstqic&password=F9pRGn6QWsgxWOXU6zf5asLqIS&ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory";
             connection = DriverManager.getConnection(URL, PROPS);
 
         } catch (SQLException ex) {
@@ -44,7 +36,7 @@ public class DBcontroller {
             return false;
         }
         
-        //connection was not found
+        // Spojenie sa nenašlo
         if (connection == null) {
             System.out.println("Connection not found.");
             return false;
@@ -57,9 +49,9 @@ public class DBcontroller {
             System.out.println(ex.toString());
         }
         
-        //Drop table at the beginning of the connection, create new one 
+        // Vymaže tabuľku pred vytvorením novej 
         List<String> querry = new ArrayList<>();
-        String record = null;
+        String record;
         String dropAllTables = "DROP TABLE isic";
         String sql = "CREATE TABLE isic " + "(id INTEGER, name VARCHAR(255), address VARCHAR(255), cardtype VARCHAR(255), latitude FLOAT, longitude FLOAT, PRIMARY KEY (id))";
 
@@ -75,14 +67,15 @@ public class DBcontroller {
             System.out.println(ex.toString());
         }
 
-        for (Iterator<GPSrecord> it = list.iterator(); it.hasNext();) {
-            GPSrecord gpsrecord = it.next();
-            record = "INSERT INTO isic VALUES (" + gpsrecord.getId() + ", '" + gpsrecord.getName() + "', '" + gpsrecord.getAddress() + "', '" + Arrays.toString(gpsrecord.getCardType()) + "', " + gpsrecord.getLatitude() + ", " + gpsrecord.getLongitude() + ");";
+        for (Iterator<Benefit> it = list.iterator(); it.hasNext();) {
+            Benefit benefit = it.next();
+            int i=1;
+            record = "INSERT INTO isic VALUES (" + i++ + ", '" + benefit.getName() + "', '" + benefit.getAddress() + "', '" + benefit.getCardAsString() + "', " + benefit.getLatitude() + ", " + benefit.getLongitude() + ");";
             querry.add(record);
-            //System.out.println(gpsrecord.toString());
+            //System.out.println(benefit.toString());
         }
         
-        //filling the DB, if there are some files
+        // Vyplnenie databáze, ak sú dostupné nejaké údaje 
         PreparedStatement pstmt;
         String str= "";
         Iterator<String> it = querry.iterator();
@@ -108,5 +101,16 @@ public class DBcontroller {
         }
         return true;
 
+    }
+    
+     public static void main(String[] args) throws ClassNotFoundException {
+        
+        Benefits bens = (Benefits) UsefullTools.loadObjectFromFile("ISIC&EYCA&SPHERE.data");
+        
+        UsefullTools.saveBenefitsToXML(bens, "ISIC&EYCA&SPHERE.xml");
+        DBcontroller cnt = new DBcontroller();
+        cnt.fillDB(bens.getBenefits());
+        /*EYCA.parserHTML();
+        SphereCard.parserHTML(); */
     }
 }
